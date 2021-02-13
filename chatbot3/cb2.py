e7945c6d6379e11eb273c3bd8949d84259a93d9d
TheBestShooter <rnolascoc@uni.pe>
2021-02-13 12:11:58 -0500

    agregando funcionalidad al carrito

--- chatbot3/cb2.py
+++ chatbot3/cb2.py
@@ -63,56 +63,137 @@ df = pd.read_csv('farmacia.csv')
 
 #cant = df.loc[df['medicamento'==y,'cantidad']]
 
-
-
 ###############################################################
 flag = True
 
 print("Chatbot: My name is chatbot. I will answer your queries about AI. If you want to exit, type Bye!")
 
+carrito = []
+
 while(flag == True):
-  user_response = input()
-  user_response = user_response.lower()
+    user_response = input()
+    user_response = user_response.lower()
   
-  producto = ''
-  if (user_response !='bye'):
-    se_encontro = 0
+    producto = ''
+    if (user_response !='bye'):
+        se_encontro = 0
     
-    user_entry = " ".join(user_response.split())
-    
-    if (user_entry.startswith("tiene")):
-        for x in df['medicamento']:
-            if (x in user_entry):
-                if se_encontro == 0:
-                    producto = x
-                    se_encontro = 1
-                elif se_encontro == 1:
-                    se_encontro = 2
+        user_entry = " ".join(user_response.split())
+        
+        if (user_entry.startswith("tiene")):
+            for x in df['medicamento']:
+                if (x in user_entry):
+                    if se_encontro == 0:
+                        producto = x
+                        se_encontro = 1
+                    elif se_encontro == 1:
+                        se_encontro = 2
 
-        if se_encontro == 0:
-            print("Lo siento, no detecté ningun producto en su mensaje")
-        elif se_encontro == 1:
-            stock = df.loc[df['medicamento'] == producto, 'cantidad'].iloc[0]
-            precio = df.loc[df['medicamento'] == producto, 'precio'].iloc[0]
-            descripcion = df.loc[df['medicamento'] == producto, 'descripcion'].iloc[0]
-            print("Chatbot: Tenemos "+ str(stock) + " en stock")
-            print("Chatbot: Precio: "+ str(precio))
-            print("Chatbot: Descripción: "+ descripcion)
-        elif se_encontro == 2:
-            print("Lo siento, solo manejo un producto al mismo tiempo, por favor solo mencione un producto")
-    
-    else:
-        if (user_response == 'thanks' or user_response == 'thank you'):
-            flag = False
-            print("Chatbot: You are welcome...")
+            if se_encontro == 0:
+                print("Lo siento, no detecté ningun producto en su mensaje")
+            elif se_encontro == 1:
+                stock = df.loc[df['medicamento'] == producto, 'cantidad'].iloc[0]
+                precio = df.loc[df['medicamento'] == producto, 'precio'].iloc[0]
+                descripcion = df.loc[df['medicamento'] == producto, 'descripcion'].iloc[0]
+                print("Chatbot: Tenemos "+ str(stock) + " en stock")
+                print("Chatbot: Precio: "+ str(precio))
+                print("Chatbot: Descripción: "+ descripcion)
+            elif se_encontro == 2:
+                print("Lo siento, solo manejo un producto al mismo tiempo, por favor solo mencione un producto")
+        
+        if (user_entry == "ver carrito"):
+            print("Carrito actual: ")
+            for i in range (0,len(carrito)):
+                print("Producto: " + str(carrito[i][0]) + "     " + "Cantidad: " + str(carrito[i][1]) + "     " + "Precio: " + str(carrito[i][2]))
+            
+            subtotal = 0
+            for value in carrito:
+                subtotal = subtotal + value[2]
+
+            print("Subtotal del carrito: " + str(subtotal))
+
+        if (user_entry.startswith("agregar")):
+            for x in df['medicamento']:
+                if (x in user_entry):
+                    if se_encontro == 0:
+                        producto = x
+                        se_encontro = 1
+                    elif se_encontro == 1:
+                        se_encontro = 2
+
+            if se_encontro == 0:
+                print("Lo siento, no detecté ningun producto en su mensaje")
+            elif se_encontro == 1:
+                array = user_entry.split(" ")
+                cantidad = array[2]
+
+                indice = 0
+
+                for index, value in df.iterrows():
+                    if value["medicamento"] == producto:
+                        indice = index
+
+                precio = df['precio'][indice] * int(cantidad)
+                carrito.append([producto, cantidad, precio])
+
+                print("El producto " + producto + " con cantidad " + cantidad + " fue agregado al carrito")
+
+                subtotal = 0
+                for value in carrito:
+                    subtotal = subtotal + value[2]
+
+                print("Subtotal del carrito: " + str(subtotal))
+                
+            elif se_encontro == 2:
+                print("Lo siento, solo se puede agregar un producto al mismo tiempo, por favor solo mencione un producto")
+
+        if (user_entry.startswith("remover")):
+            for x in df['medicamento']:
+                if (x in user_entry):
+                    if se_encontro == 0:
+                        producto = x
+                        se_encontro = 1
+                    elif se_encontro == 1:
+                        se_encontro = 2
+
+            if se_encontro == 0:
+                print("Lo siento, no detecté ningun producto en su mensaje")
+            elif se_encontro == 1:
+
+                se_borro = False
+                index = 0
+
+                #Borrar el producto del carrito
+                while index < len(carrito):
+                    if carrito[index][0] == producto:
+                        carrito.pop(index)
+                        index = index - 1
+                        se_borro = True
+
+                    index = index + 1
+                        
+
+                if(se_borro):
+                    print("El producto " + producto + " se borró exitosamente del carrito")
+
+                else:
+                    print("Lo siento, el producto " + producto + " no se encontró en su carrito")
+                
+            elif se_encontro == 2:
+                print("Lo siento, solo se puede agregar un producto al mismo tiempo, por favor solo mencione un producto")
+
         else:
-            if (greeting(user_response) != None):
-                print("Chatbot:" + greeting(user_response))
+            if (user_response == 'thanks' or user_response == 'thank you'):
+                flag = False
+                print("Chatbot: You are welcome...")
             else:
-                print("Chatbot:",end = "") 
-                print(response(user_response))
-                sent_tokens.remove(user_response)
+                if (greeting(user_response) != None):
+                    print("Chatbot:" + greeting(user_response))
+                else:
+                    print("Chatbot:",end = "") 
+                    print(response(user_response))
+                    sent_tokens.remove(user_response)
 
-  else:
-    flag = False 
-    print("Chatbot: Bye! take care...")
+    else:
+        flag = False 
+        print("Chatbot: Bye! take care...")
