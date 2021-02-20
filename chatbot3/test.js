32f25a2b7edae4aecdef645302a5ee7226baf6cc
TheBestShooter <rnolascoc@uni.pe>
2021-02-20 02:20:39 -0500

    script para el dialogflow

---
+++ chatbot3/test.js
@@ -0,0 +1,89 @@
+// Programmer: Cesar Romero
+// Development: Chatbot365
+'use strict';
+// Sigueme en mi grupo de facebook
+// https://www.facebook.com/groups/chatbot365
+// Si quieres que te ayude con algo unete a telegram
+// https://t.me/chatbot365
+
+const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+const axios = require('axios');
+
+
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+
+    function crearProducto(agent) {
+    	const nombrecr = agent.parameters.nombre;
+    	const preciocr = agent.parameters.precio;
+    	axios.post('https://sheetdb.io/api/v1/e3m8zd1m5y4ad',{
+	    	"data":[{ "nombre": nombrecr, "precio": preciocr }]
+    	}).then( response => {
+      		console.log(response.data);
+    	});
+
+    	agent.add('El producto fue registrado');
+    }
+
+    function verProductos(agent) {
+        axios.get('https://sheetdb.io/api/v1/e3m8zd1m5y4ad').then( response => {
+          	console.log(response.data);
+        });
+        	agent.add("HOLAAA");
+        }
+
+  // // Uncomment and edit to make your own intent handler
+  // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
+  // // below to get this function to be run when a Dialogflow intent is matched
+  // function yourFunctionHandler(agent) {
+  //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
+  //   agent.add(new Card({
+  //       title: `Title: this is a card title`,
+  //       imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
+  //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! ðŸ’`,
+  //       buttonText: 'This is a button',
+  //       buttonUrl: 'https://assistant.google.com/'
+  //     })
+  //   );
+  //   agent.add(new Suggestion(`Quick Reply`));
+  //   agent.add(new Suggestion(`Suggestion`));
+  //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
+  // }
+
+  // // Uncomment and edit to make your own Google Assistant intent handler
+  // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
+  // // below to get this function to be run when a Dialogflow intent is matched
+  // function googleAssistantHandler(agent) {
+  //   let conv = agent.conv(); // Get Actions on Google library conv instance
+  //   conv.ask('Hello from the Actions on Google client library!') // Use Actions on Google library
+  //   agent.add(conv); // Add Actions on Google library responses to your agent's response
+  // }
+  // // See https://github.com/dialogflow/fulfillment-actions-library-nodejs
+  // // for a complete Dialogflow fulfillment library Actions on Google client library v2 integration sample
+
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  // intentMap.set('your intent name here', yourFunctionHandler);
+  // intentMap.set('your intent name here', googleAssistantHandler);
+  intentMap.set('crearProducto', crearProducto);
+  intentMap.set('verProductos', verProductos);
+
+  agent.handleRequest(intentMap);
+});
